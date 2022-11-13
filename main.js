0c35a7cc5a77a42687a7b4271da3c5e1a6463af7
salaheddine797 <se30533@gmail.com>
2022-11-13 20:03:57 +0000

    finished

---
+++ main.js
@@ -0,0 +1,30 @@
+const submit = document.getElementById('submit');
+const rates = document.querySelectorAll('.btn');
+const thanksContainer = document.querySelector('.thanksContainer');
+const ratingContainer = document.querySelector('.container');
+const ratingButton = document.getElementById('RateAgain')
+const numberRated = document.getElementById('rated');
+
+
+
+
+submit.addEventListener('click', () => {
+    thanksContainer.classList.remove('hidden');
+    ratingContainer.style.display = "none";
+});
+
+ratingButton.addEventListener('click', () => {
+    thanksContainer.classList.add('hidden');
+    ratingContainer.style.display = "block";
+});
+
+
+
+rates.forEach(rate => {
+    rate.addEventListener('click',()=>{
+        numberRated.innerHTML = rate.innerHTML;
+    })
+});
+
+
+
